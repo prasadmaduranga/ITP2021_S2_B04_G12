e712dcdbef1c76aef922c2e6983ca64e6980d1a0
it20228644 <it20228644@my.sliit.lk>
2021-09-06 22:31:31 +0530

    initial commit

---
+++ src/main/java/lk/sliit/hotel/dao/TimeDAO.java
@@ -0,0 +1,43 @@
+package lk.sliit.hotel.dao;
+
+import lk.sliit.hotel.entity.TimeCheck;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+
+public interface TimeDAO extends CrudRepository<TimeCheck, String> {
+
+    //Iterable<OnlineTableReservation> findOnlineTableReservationsByStartTimeBeforeAndEndTimeAfterAndDateEquals(Time startTime, Time endTime, Date date);
+
+   // Iterable<OnlineTableReservation> findOnlineTableReservationsByDateEquals(Date date);
+
+
+   //Iterable<TimeCheck> findTimeChecksByTimeSettBeforeAndTimeSett2AfterAndDateEquals(Date tdate, Date tdate2, Date date);
+
+
+//    @Query(value = "SELECT e FROM TimeCheck e WHERE e.timeSett BETWEEN :startDate AND :endDate order by e.joinDate")
+//   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Iterable<TimeCheck> findAllByTimeTwoGreaterThanEqualAndTimeOneLessThanEqualAndDateEquals(Date tdate, Date tdate2, Date date);
+    Iterable<TimeCheck> findAllByTimeTwoBetweenAndDateEquals(Date tdate, Date tdate2, Date date);
+    Iterable<TimeCheck> findAllByTimeOneBetweenAndDateEquals(Date tdate, Date tdate2, Date date);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//    Iterable<TimeCheck> findAllByTimeTwoLessThanEqualAndTimeOneGreaterThanEqualAndDateEquals(Date tdate2, Date tdate, Date date);
+//    Iterable<TimeCheck> findAllByTimeOneLessThanEqualAndTimeTwoGreaterThanEqualAndDateEquals(Date tdate2, Date tdate, Date date);
+
+//    @Query(value =" from TimeCheck where ((timeOne between  timeOne and timeTwo) or (timeTwo between timeOne and timeTwo))")
+//    Iterable<TimeCheck> uals(Date tdate,Date tdate2, Date date);
+
+
+    //Iterable<TimeCheck> findTimeChecksByTimeOneIsInOrTimeTwoIsInAndDateEquals(Date tdate, Date tdate2, Date date);
+
+   // Iterable<TimeCheck>  findTimeChecksByTimeOneSinceGreaterThanEqualAndUntilLessThanEqual(Long since, Long until);
+
+   // Iterable<TimeCheck> findAllByTimeOneAndTimeTwoNotContainingInTimeOneGreaterThanEqualAndTimeTwoLessThanEqualAndDateEquals(Date tdate, Date tdate2, Date date);
+
+//    @Query(value = "FROM TimeCheck e WHERE e.timeOne  BETWEEN e.timeOne AND e.timeTwo",nativeQuery = false)
+//    Iterable<TimeCheck> getAllBetweenDates( @Param("startDate")Date tdate, @Param("endDate")Date tdate2,@Param("date")Date date);
+}
+
+  /*  @Query( value = "SELECT admin FROM emp2.Employee WHERE idNo=?1",nativeQuery = true)
+    boolean isAdmin(String idNo);*/
