4746b3aeed89b59de25ff37eb6283f14475c3695
it20228644 <it20228644@my.sliit.lk>
2021-09-06 20:07:32 +0530

    initial commit

---
+++ src/main/java/lk/sliit/hotel/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.sliit.hotel;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableJpaRepositories
+public class AppInitializer {
+    public static void main(String[] args) {
+
+
+    }
+}
