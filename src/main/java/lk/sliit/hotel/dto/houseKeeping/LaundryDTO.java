36dc90aaf7ac5d6667cd2777a34f8b2ecf2dfa43
Kethani <86484666+Kethani@users.noreply.github.com>
2021-09-11 13:08:59 -0700

    LaundryDTO added

---
+++ src/main/java/lk/sliit/hotel/dto/houseKeeping/LaundryDTO.java
@@ -0,0 +1,119 @@
+package lk.sliit.hotel.dto.houseKeeping;
+
+
+import java.sql.Date;
+
+public class LaundryDTO {
+
+    private int laundryId;
+    private int customerId;
+    private String customerName;
+    private int orderHolder;
+    private double  pieces;
+    private Date expectedDate;
+    private Date date;
+    private String state;
+
+    public LaundryDTO() {}
+
+    public LaundryDTO(int laundryId, int customerId, int orderHolder, double pieces, Date expectedDate, Date date) {
+        this.laundryId = laundryId;
+        this.customerId = customerId;
+        this.orderHolder = orderHolder;
+        this.pieces = pieces;
+        this.expectedDate = expectedDate;
+        this.date = date;
+    }
+
+    public LaundryDTO(int laundryId, int customerId, String customerName, int orderHolder, double pieces, Date expectedDate, Date date, String state) {
+        this.laundryId = laundryId;
+        this.customerId = customerId;
+        this.customerName =customerName;
+        this.orderHolder = orderHolder;
+        this.pieces = pieces;
+        this.expectedDate = expectedDate;
+        this.date = date;
+        this.state = state;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LaundryDTO(int laundryId) {
+        this.laundryId = laundryId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getOrderHolder() {
+        return orderHolder;
+    }
+
+    public void setOrderHolder(int orderHolder) {
+        this.orderHolder = orderHolder;
+    }
+
+    public double getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(double pieces) {
+        this.pieces = pieces;
+    }
+
+    public Date getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(Date expectedDate) {
+        this.expectedDate = expectedDate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getLaundryId() {
+        return laundryId;
+    }
+
+    public void setLaundryId(int laundryId) {
+        this.laundryId = laundryId;
+    }
+
+    @Override
+    public String toString() {
+        return "LaundryDTO{" +
+                "laundryId=" + laundryId +
+                ", customerId=" + customerId +
+                ", orderHolder=" + orderHolder +
+                ", pieces=" + pieces +
+                ", expectedDate=" + expectedDate +
+                ", date=" + date +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
