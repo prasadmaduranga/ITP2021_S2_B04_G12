0bc92bf3e3d73a170376c91195e7d237a326f7f6
shalinikaMalshani <it20233976@my.sliit.lk>
2021-09-12 13:21:32 +0530

    add restaurant online order entity and dto

---
+++ src/main/java/lk/sliit/hotel/dto/restaurant/restaurantOnlineOrder/RestaurantOnlineOrderDetailsDTO.java
@@ -0,0 +1,61 @@
+package lk.sliit.hotel.dto.restaurant.restaurantOnlineOrder;
+
+
+
+public class RestaurantOnlineOrderDetailsDTO {
+    private String restaurantOnlineOrder;
+    private int foodItem;
+    private double quantity;
+    private double unitePrice;
+
+    public RestaurantOnlineOrderDetailsDTO(String restaurantOnlineOrder, int foodItem, double quantity, double unitePrice) {
+        this.restaurantOnlineOrder = restaurantOnlineOrder;
+        this.foodItem = foodItem;
+        this.quantity = quantity;
+        this.unitePrice = unitePrice;
+    }
+
+    public RestaurantOnlineOrderDetailsDTO() {
+    }
+
+    public String getRestaurantOnlineOrder() {
+        return restaurantOnlineOrder;
+    }
+
+    public void setRestaurantOnlineOrder(String restaurantOnlineOrder) {
+        this.restaurantOnlineOrder = restaurantOnlineOrder;
+    }
+
+    public int getFoodItem() {
+        return foodItem;
+    }
+
+    public void setFoodItem(int foodItem) {
+        this.foodItem = foodItem;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitePrice() {
+        return unitePrice;
+    }
+
+    public void setUnitePrice(double unitePrice) {
+        this.unitePrice = unitePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "RestaurantOnlineOrderDetailsDTO{" +
+                "foodItem='" + foodItem + '\'' +
+                ", quantity=" + quantity +
+                ", unitePrice=" + unitePrice +
+                '}';
+    }
+}
