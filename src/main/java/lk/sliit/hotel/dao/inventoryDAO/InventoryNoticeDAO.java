b703b7f958130d9dac87a38753640b65fa962470
jayasinghe0000 <it20241650@my.sliit.lk>
2021-09-15 09:53:42 +0530

    InventoryNoticeDAO aded

---
+++ src/main/java/lk/sliit/hotel/dao/inventoryDAO/InventoryNoticeDAO.java
@@ -0,0 +1,25 @@
+package lk.sliit.hotel.dao.inventoryDAO;
+
+import lk.sliit.hotel.entity.inventory.Inventory;
+import lk.sliit.hotel.entity.inventory.InventoryNotice;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+
+public interface InventoryNoticeDAO extends CrudRepository<InventoryNotice,Integer> {
+
+
+
+    InventoryNotice findTopByOrderByNoticeIdDesc();
+
+    InventoryNotice findInventoryNoticeByInventoryAndExpDateEquals(Inventory noticeId, java.sql.Date date);
+
+    Iterable<InventoryNotice> findAllByDateBetween(Date today, Date beforeWeek);
+
+    Iterable<InventoryNotice> findAllByDateBetweenAndDepartmentEquals(Date beforeweek, Date todaya,String dep);
+
+    InventoryNotice findInventoryNoticeByExpDateAndDepartmentAndInventoryAndStateEquals(java.sql.Date date, String department, Inventory id, boolean state);
+
+    Iterable<InventoryNotice> findAllByExpDateAndStateEquals(Date tomorrow, boolean b);
+
+}
