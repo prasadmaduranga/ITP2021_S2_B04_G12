b7c544d19d4dc4f88eb32bb1dd0844b3e757d16c
sajiniPW <it20418274@my.sliit.lk>
2021-09-17 16:09:41 +0530

    reservationDetailsPK added

---
+++ src/main/java/lk/sliit/hotel/entity/reservation/ReservationDetailsPK.java
@@ -0,0 +1,35 @@
+package lk.sliit.hotel.entity.reservation;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ReservationDetailsPK implements Serializable {
+
+    private int reservationId;
+    private int roomId;
+
+    public ReservationDetailsPK(int reservationId, int roomId) {
+        this.reservationId = reservationId;
+        this.roomId = roomId;
+    }
+
+    public ReservationDetailsPK() {
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+}
